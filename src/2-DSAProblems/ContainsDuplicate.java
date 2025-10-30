4ddeca10ba29c0724e69ea06d204f721d16fb0b0
Diego Hurtado <dhurtado@evergreen.shipping.argentina>
2025-10-30 20:08:39 -0300

    DSA Problems

---
+++ src/2-DSAProblems/ContainsDuplicate.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+
+        int[] array = {1,2,3,4,5,6,7,8,9};
+        boolean result = false;
+        HashSet <Integer> map = new HashSet<>();
+
+        for (int i = 0; i < array.length; i++) {
+            if(map.contains(array[i])){
+                result = true;
+                break;
+            }
+            map.add(array[i]);
+        }
+        System.out.println("El valor de result es: " + result);
+    }
+}
